5a231d7fa65c7b0c29edcfe114beb0ea8dbc0b22
kay2025-max <kayruathanstore2025@gmail.com>
2025-08-27 12:41:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ intents.message_content = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 # --- CONFIG ---
 CHANNEL_ID = 1407910664621789234   # kênh gửi panel
